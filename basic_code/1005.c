9ebdc30d2b8521bc5678f2c6223afeeeab08a9a1
Hwang <Hwang@DESKTOP-HN88IJO>
2023-07-03 23:13:42 +0900

    초기 커밋.

---
+++ basic_code/1005.c
@@ -0,0 +1,8 @@
+#define CRT_SECURE_NO_WARINGS
+#include <stdio.h>
+
+int main()
+{
+	printf("\"Hello World\"");
+	return 0;
+}
---
+++ basic_code/1007.c
@@ -0,0 +1,8 @@
+#define CRT_SECURE_NO_WARINGS
+#include <stdio.h>
+
+int main()
+{
+	printf("\"C:\\Download\\hello.cpp\"");
+	return 0;
+}
