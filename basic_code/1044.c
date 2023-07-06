e96094eef24a2fe5563b99e2045e7d01543a586b
Hwang <Hwang@DESKTOP-HN88IJO>
2023-07-06 22:34:58 +0900

    2023.07.06

---
+++ basic_code/1044.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	long long int a;
+	scanf("%lld", &a);
+	printf("%lld", a + 1);
+}
---
+++ basic_code/1046.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%d\n", a + b + c);
+	printf("%.1f", (float)(a + b + c) / 3);
+}
